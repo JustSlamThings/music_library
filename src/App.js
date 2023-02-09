cd11cc137bbe167c35b237d8eefacaec8630fad3
Stephen Lam <justslamthings@gmail.com>
2023-02-08 21:05:21 -0500

    redoing these assignments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
 import { DataContext } from './context/DataContext'
-
+import { SearchContext } from './context/SearchContext'
 
 
 
@@ -11,14 +11,21 @@ const App = () => {
     let [search, setSearch] = useState('')
     let [message, setMessage] = useState('Search for Music!')
     let [data, setData] = useState([])
+    let searchInput = useRef('')
 
     const API_URL = 'https://itunes.apple.com/search?term='
 
     useEffect(() => {
         if(search) {
-            const fetchData = async () => {
-                document.title = `${search} music`
-                const response = await fetch(API_URL + search)
+            
+        }
+    }, [search])
+
+    const handleSearch = (e, term) => {
+        e.preventDefault()
+        const fetchData = async () => {
+                document.title = `${term} music`
+                const response = await fetch(API_URL + term)
                 const resData = await response.json()
                 if (resData.results.length > 0) {
                     return setData(resData.results)
@@ -27,17 +34,17 @@ const App = () => {
                 }
             }
             fetchData()
-        }
-    }, [search])
-
-    const handleSearch = (e, term) => {
-        e.preventDefault()
         setSearch(term)
     }
 
     return (
         <div className='App'>
-            <SearchBar handleSearch={handleSearch} />
+            <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+            }}>
+                <SearchBar />
+            </SearchContext.Provider>
             {message}
             <DataContext.Provider value={data} >
                 <Gallery />
